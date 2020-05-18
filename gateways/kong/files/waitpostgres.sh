24bea3424024b70d7148034a7f478c696af17536
Sven Walther <sven@svenwal.de>
2020-05-18 17:35:33 +0200

    Some work on making ot work in 2020 - still much to do...

--- gateways/kong/files/waitpostgres.sh
+++ gateways/kong/files/waitpostgres.sh
@@ -6,16 +6,16 @@ timelimit=180
 timeout=$timelimit
 while [ $check -gt 0 ]
 do
-    check=`nc 127.0.0.1 9042 < /dev/null 2> /dev/null; echo $?`
+    check=`nc 127.0.0.1 5432 < /dev/null 2> /dev/null; echo $?`
     timeout=$(( $timeout - 1 ))
     if [ $timeout -eq 0 ]
     then
-        echo "Cassandra not yet up, aborting."
+        echo "Postgres not yet up, aborting."
         exit 1
     fi
     sleep 1s
 done
 
 timediff=$(( $timelimit - $timeout ))
-echo "Cassandra started after $timediff seconds"
+echo "Postgres started after $timediff seconds"
 exit 0
